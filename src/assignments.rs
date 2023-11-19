56ce7952d7964afcdb3ed32d1da31baa926d60e0
shegx01 <shegx01@gmail.com>
2023-11-20 00:05:24 +0200

    properly arrange to modules

---
+++ src/assignments.rs
@@ -0,0 +1,2 @@
+pub mod recursion;
+pub use recursion::*;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
+
+
+mod assignments;
 fn main() {
-    println!("{}", crate::sum_of_digits(221432784576))
-}
 
-pub fn sum_of_digits(num: u64) -> u64 {
-    // for an input that is 21,
-    // 1 + 2 + 0
-    return if num == 0 {
-        0
-    } else {
-        (num % 10) + sum_of_digits(num / 10)
-    };
-
-    // Elixir Version
-    // require Integer
-    // def sum_of_digit(n) when is_integer(n) and n >= 0, do: sum_digit(n)
-    // defp sum_digit(0), do: 0
-    // defp sum_digit(n), do: Integer.mod(n, 10) + sum_digit(div(n, 10))
+    println!("{}", assignments::sum_of_digits(221432784576))
 }
