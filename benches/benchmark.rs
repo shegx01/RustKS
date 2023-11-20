f9d164260a9fb3af3458c4b32098f6bf6fad947d
shegx01 <shegx01@gmail.com>
2023-11-20 10:24:06 +0200

    Added sum of digits

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,15 +2,21 @@ use concur::assignments;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn sum_of_digits_rec(c: &mut Criterion) -> &mut Criterion {
-    c.bench_function("assignment", |b| {
+    c.bench_function("sum_of_digits_rec", |b| {
         b.iter(|| assignments::sum_of_digits(black_box(32643565)))
-    })
-}
-pub fn sum_of_digits_optimized(c: &mut Criterion) -> &mut Criterion {
-    c.bench_function("assignment", |b| {
+    });
+    c.bench_function("sum_of_digits_optimized", |b| {
         b.iter(|| assignments::sum_of_digits_optimized(black_box(32643565)))
     })
 }
+pub fn pow(c: &mut Criterion) -> &mut Criterion {
+    c.bench_function("pow_rec", |b| {
+        b.iter(|| assignments::pow_rec(black_box(32), black_box(3)))
+    });
+    c.bench_function("pow", |b| {
+        b.iter(|| assignments::pow(black_box(35), black_box(21)))
+    })
+}
 
-criterion_group!(benches, sum_of_digits_rec, sum_of_digits_optimized);
+criterion_group!(benches, sum_of_digits_rec, pow);
 criterion_main!(benches);
--- src/assignments/recursion.rs
+++ src/assignments/recursion.rs
@@ -24,6 +24,24 @@ pub fn sum_of_digits_optimized(n: u64) -> u64 {
     sum_digits(n, 0)
 }
 
+pub fn pow_rec(base: u64, exp: u32) -> u64 {
+   return if exp == 0 { 1 }
+    else { base * pow(base, exp -1) as u64 }
+}
+
+pub fn pow(base: u64, exp: u32) -> u64 {
+    // For some reasons,
+    // This is slower than the direct counterpart
+    fn calc_pow(base: u64, exp: u32, result: u64) -> u64 {
+        return if exp == 0 {
+            result
+        } else {
+            calc_pow(base, exp - 1, result * base)
+        }
+    }
+
+    calc_pow(base, exp, 1)
+}
 #[test]
 fn sum_of_digits_rec_test() {
     assert_eq!(sum_of_digits(21), 3);
@@ -35,4 +53,17 @@ fn sum_of_digits_optimized_test() {
     assert_eq!(sum_of_digits_optimized(21), 3);
     assert_eq!(sum_of_digits_optimized(2221), 7);
     assert_eq!(sum_of_digits_optimized(7120), 10);
+}
+
+#[test]
+pub fn pow_test_rec() {
+    assert_eq!(pow_rec(2, 4), 16);
+    assert_eq!(pow_rec(3, 3), 27)
+}
+
+
+#[test]
+pub fn pow_test() {
+    assert_eq!(pow(2, 4), 16);
+    assert_eq!(pow(3, 3), 27)
 }
